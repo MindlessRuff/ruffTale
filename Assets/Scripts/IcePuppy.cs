28637859574898559d19cc8487cfaec5b66b614d
Brandon Veltre <40220862+MindlessRuff@users.noreply.github.com>
2020-01-25 13:59:06 -0800

    Create Initial Scene with Character Movement

---
+++ Assets/Scripts/IcePuppy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class IcePuppy : MonoBehaviour
+{
+    public GameObject dialogBox;
+    TextMeshPro dialogText;
+    int dialogCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogBox.SetActive(false);
+        dialogCount = 0;
+        dialogText = GetComponent<TextMeshPro>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void advanceDialog() {
+        if (dialogCount == 0)
+        {
+            dialogBox.SetActive(true);
+            dialogText.text = "Success"; 
+        }
+    }
+}
---
+++ Assets/Scripts/MainCharacterController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Vector2 lookDirection = new Vector2(1, 0);
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Store User Input to calculate position
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+        // Store the current position of the player to get the difference
+        Vector2 position = rigidbody2d.position;
+        position = position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                IcePuppy puppy = hit.collider.GetComponent<IcePuppy>();
+                if (puppy != null)
+                {
+                    Debug.Log("Advance Dialog");
+                    puppy.advanceDialog();
+                }
+            }
+        }
+
+    }
+}
